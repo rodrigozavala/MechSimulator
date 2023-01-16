560132c833f83f2009f3fbc45e358e89111afaec
Rodrigo <lobo.negroguionbajo3@gmail.com>
2023-01-15 18:11:55 -0600

    At least now it shows something on screen, but I'm still figuring out what does this means, Links do not appear as I want them to appear, it seems like I need to make link3.p0=link2.pf but for some reason this does not work so well

--- mech.py
+++ mech.py
@@ -1,7 +1,9 @@
 import pygame, sys, time
 import numpy as np
 import sympy as sym
+import math
 from links import SimpleLink
+from links import Mechanism
 
 #Defining variables
 t1,t2,t3,t4=sym.symbols("t_1 t_2 t_3 t_4")
@@ -11,38 +13,53 @@ a1,a2,a3=sym.symbols("a_1 a_2 a_3")
 l1=7
 l2=15
 l3=7
-l4=10 #this is ground
+l4=15 #this is ground
 
-theta1=3.1415*0.25
-omega1=3.1415*0.3
-alfa1=0
-theta4=0.5
-link1=SimpleLink(d=l1,theta=0.01745329)
-link2=SimpleLink(d=l1,theta=0.01745329)
-link3=SimpleLink(d=l1,theta=0.01745329)
-link4Ground=SimpleLink(d=l1,theta=0.01745329)
-angles=[{"theta":(i*0.01745329),"omega":3.1415*0.3,"alfa":0} for i in range(1,361)]
-solutions=[[0.7,1,3.1415*0.3,3.1415*0.3,0,0]]
+theta1=math.pi*0.312832
+omega1=0.3*math.pi
+alfa1=0.02
+theta4=math.pi*0.5288
+
+
+angles=[{"theta":((i*0.017453292)+theta1),"omega":omega1,"alfa":alfa1} for i in range(0,39)]
+solutions=[[-0.09065,math.pi*1.312832,3.1415*0.03,3.1415*0.3,0,0]]
 i=0
 for value in angles:
     print("STARTING CALCULATION")
+    
     ec1=sym.Eq(l1*sym.cos(t1)+l2*sym.cos(t2)+l3*sym.cos(t3)+l4*sym.cos(t4),0).subs(t4,theta4).subs(t1,value["theta"])
-    ec2=sym.Eq(l1*sym.sin(t1)+l2*sym.sin(t2)+l3*sym.sin(t3)+l4*sym.sin(t4),0).subs(t4,theta4).subs(t1,value["theta"])
+    ec2=sym.Eq(l1*sym.sin(t1)+l2*sym.sin(t2)+l3*sym.sin(t3)+l4*sym.sin(t4),0).subs(t4,theta4).subs(t1,value["theta"])##think if I need to change theta4 after the first simulation
+    ##do not change theta4, its ground angle
+    
     ec3=sym.Eq(-l1*w1*sym.sin(t1)-l2*w2*sym.sin(t2)-l3*w3*sym.sin(t3),0).subs(t1,value["theta"]).subs(w1,value["omega"])
     ec4=sym.Eq(l1*w1*sym.cos(t1)+l2*w2*sym.cos(t2)+l3*w3*sym.cos(t3),0).subs(t1,value["theta"]).subs(w1,value["omega"])
+    
     ec5=sym.Eq(-w1**2*l1*sym.cos(t1)-w2**2*l2*sym.cos(t2)-w3**2*l3*sym.cos(t3)-a1*l1*sym.sin(t1)-a2*l2*sym.sin(t2)-a3*l3*sym.sin(t3),0).subs(t1,value["theta"]).subs(w1,value["omega"]).subs(a1,value["alfa"])
     ec6=sym.Eq(-w1**2*l1*sym.sin(t1)-w2**2*l2*sym.sin(t2)-w3**2*l3*sym.sin(t3)+a1*l1*sym.cos(t1)+a2*l2*sym.cos(t2)+a3*l3*sym.cos(t3),0).subs(t1,value["theta"]).subs(w1,value["omega"]).subs(a1,value["alfa"])
     i+=1
     print(i,solutions[-1])
-    something=sym.nsolve([ec1,ec2,ec3,ec4,ec5,ec6],[t2,t3,w2,w3,a2,a3],solutions[-1],verify=False)
-    #print(something)
-    #print(something[:])
+    something=sym.nsolve([ec1,ec2,ec3,ec4,ec5,ec6],[t2,t3,w2,w3,a2,a3],solutions[-1])##,verify=False)
     solutions.append(something[:])
 
-print(solutions)
-solutions[0]=[]
-l=SimpleLink(1,2)
-print(l.x0,l.y0)
+#Changing angles
+solutions = solutions [1:]
+#link4Ground.setTheta(theta4)
+if(len(angles) == len(solutions)):
+    print("AAHHHHHHHHHHHHHH THEY ARE EQUAL IN LENGTH")
+
+else:
+    print(len(angles)," ",len(solutions))
+
+"""
+for i in len(solutions):
+    link1.updatepFWithTheta(angles[i]["theta"])
+    link2.setP0(link1.pf)
+    link2.updatepFWithTheta(solutions[i][0])
+    link3.setP0(link2.pf)
+    link3.updatepFWithTheta(solutions[i][1])
+    link4Ground.setP0(link3.pf)
+"""
+
 #Defining some colors
 WHITE=(255,255,255)
 CYAN=(0, 251, 255)
@@ -71,13 +88,15 @@ LIGHT_BRONW=(140, 111, 62)
 pygame.init()
 size=(800,500)
 screen=pygame.display.set_mode(size)
+"""
 rotation=lambda t: np.array([[np.cos(t),-np.sin(t)],[np.sin(t),np.cos(t)]])
 rot=lambda u,t: np.transpose(np.matmul(rotation(t),np.array([[u[0]],[u[1]]])))
 rot_point=lambda u,t,p:rot(u-p,t)+p
 u0=np.array([0,100])
 u1=np.array([200,100])
-p=np.array([100,100])
-
+p=np.array([100,100])"""
+i=0
+firstTime=True
 while True:
     for event in pygame.event.get():
         print(event)
@@ -90,20 +109,39 @@ while True:
     screen.fill(LIGHT_GRAY)
     #update screen
     #print("####jajaja")
-    pygame.draw.line(screen,GREEN,u0,u1,5)
+    #pygame.draw.line(screen,GREEN,u0,u1,5)
     pygame.draw.line(screen,RED,[0,0],[0,100],5)
     pygame.draw.line(screen,RED,[0,0],[100,0],5)
 
+
+    initx0=100
+    inity0=100
+
+    sFactor=10
+    if(firstTime):
+        link1=SimpleLink(x0=initx0,y0=inity0,d=l1*sFactor,theta=angles[i]["theta"])
+        link2=SimpleLink(x0=link1.pf.p[0],y0=link1.pf.p[1],d=l2*sFactor,theta=solutions[i][0])
+        link3=SimpleLink(x0=link2.pf.p[0],y0=link2.pf.p[1],d=l3*sFactor,theta=solutions[i][1])
+        link4Ground=SimpleLink(x0=link3.pf.p[0],y0=link3.pf.p[1],xf=initx0,yf=inity0,d=l4*sFactor,theta=theta4)
+        
+        firstTime=False
     
-    #u0=rot(u0,0.05)
-    #u0=rot_point(u0,0.05,p)
-    #u0=[u0[0,0],u0[0,1]]
-    #print(u0)
+    link1.updatepFWithTheta(angles[i]["theta"])
+    pygame.draw.line(screen,RED,link1.p0.p,link1.pf.p,5)
+    link2.setP0(link1.pf)
+    #link2.updatepFWithTheta(solutions[i][0])
+    pygame.draw.line(screen,BLACK,link2.p0.p,link2.pf.p,5)
+    link3.updatepFWithTheta(solutions[i][1])
+    #link3.setP0(link2.pf)
+    pygame.draw.line(screen,GREEN,link3.p0.p,link3.pf.p,5)
     
-    #u1=rot(u1,0.05)
-    #u1=rot_point(u1,0.05,p)
-    #u1=[u1[0,0],u1[0,1]]
-    #print(u1)
+    #link4Ground.setP0(link3.pf)
+    pygame.draw.line(screen,BROWN,link4Ground.p0.p,link4Ground.pf.p,5)
+
+    i+=1
+    if(i==len(solutions)):
+        firstTime=True
+        i=0
     pygame.display.flip()
     time.sleep(1/30)
 
